708f2e5a3a248cf8eb5fff2f2e6f872de42fdbca
Wade Desir <wededesir@gmail.com>
2022-08-31 17:29:07 -0400

    second install of nextui

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8070cb261bfe752b"; }
+/******/ 		__webpack_require__.h = function() { return "2d6a7a70eb575506"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
@@ -1222,11 +1222,6 @@
 /******/ 		var chunkLoadingGlobal = self["webpackChunk_N_E"] = self["webpackChunk_N_E"] || [];
 /******/ 		chunkLoadingGlobal.forEach(webpackJsonpCallback.bind(null, 0));
 /******/ 		chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
-/******/ 	}();
-/******/ 	
-/******/ 	/* webpack/runtime/nonce */
-/******/ 	!function() {
-/******/ 		__webpack_require__.nc = undefined;
 /******/ 	}();
 /******/ 	
 /************************************************************************/
--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,13 @@
-import { useState } from "react";
+import * as React from "react";
 
-import Nav from "../components/Nav.js";
+// 1. import `NextUIProvider` component
+import { NextUIProvider } from "@nextui-org/react";
 
-function Header({ title }) {
-  return <h1>{title ? title : "Default title"}</h1>;
-}
-
-export default function HomePage() {
-  const names = ["Ada Lovelace", "Grace Hopper", "Margaret Hamilton"];
-
-  const [likes, setLikes] = useState(0);
-
-  function handleClick() {
-    setLikes(likes + 1);
-  }
-
+function App({ Component }) {
+  // 2. Use at the root of your app
   return (
-    <div>
-      <Nav />
-      <Header title="Develop. Preview. Ship. 🚀" />
-      <ul>
-        {names.map((name) => (
-          <li key={name}>{name}</li>
-        ))}
-      </ul>
-
-      <button onClick={handleClick}>Like ({likes})</button>
-    </div>
+    <NextUIProvider>
+      <Component />
+    </NextUIProvider>
   );
 }
